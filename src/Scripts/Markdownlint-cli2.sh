93919c5e7367bf599cc2564ea7b9a42f511746bc
Tom King <mobilemind@users.noreply.github.com>
2025-05-15 20:19:40 -0700

    feat(script): streamline calls & internal config/error checks

--- src/Scripts/Markdownlint-cli2.sh
+++ src/Scripts/Markdownlint-cli2.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# shellcheck disable=SC2181
+
 SHORTNAME="$(basename "$BB_DOC_PATH")"
 SCRIPT="$(basename "$0" '.sh')"
 SCRIPTDIR="$(pwd)"
@@ -10,17 +12,10 @@ NOTIFY() { # $1=Title, $2=Message, $3=Sound
 	(nohup osascript "$ASNOTIFY" "$1" "$2" "$3" >/dev/null 2>&1 &)
 }
 
-# check for npx
-if ! hash npx > /dev/null 2>&1 ; then
-	NOTIFY "$SCRIPT: ERROR" "npx not found. Try installing npm. Then restart BBedit." 'sosumi'
-	echo "ERROR: npx not found. Try installing npm. Then restart BBedit. (v$VERSION)"
-	exit 1
-fi
-
 # check for markdownlint-cli2
 if ! hash markdownlint-cli2 > /dev/null 2>&1 ; then
-	NOTIFY "$SCRIPT: ERROR" "markdownlint-cli2 not installed. Try installing it with 'npm install -g markdownlint-cli2'. Then restart BBedit." 'sosumi'
-	echo "ERROR: markdownlint-cli2 not installed. Try installing it with 'npm install -g markdownlint-cli2' then restart BBEdit. (v$VERSION)"
+	NOTIFY "$SCRIPT: ERROR" "markdownlint-cli2 not found. Try installing it with 'npm install -g markdownlint-cli2'. Then restart BBEdit." 'sosumi'
+	echo "ERROR: markdownlint-cli2 not found. Try installing it with 'npm install -g markdownlint-cli2' then restart BBEdit. (v$VERSION)"
 	exit 1
 fi
 
@@ -32,50 +27,37 @@ if [ ! -s "${BB_DOC_PATH}" ] ; then
 fi
 
 # if BB_DOC_PATH is within $BB_DOC_WORKSPACE_ROOT, then make path more readable
-if [ -n "$BB_DOC_WORKSPACE_ROOT" ] && [ "${BB_DOC_PATH#*"$BB_DOC_WORKSPACE_ROOT"}" != "$BB_DOC_PATH" ]; then
+if [ -n "$BB_DOC_WORKSPACE_ROOT" ] && [ "${BB_DOC_PATH#*"$BB_DOC_WORKSPACE_ROOT"}" != "$BB_DOC_PATH" ] ; then
 	cd "$BB_DOC_WORKSPACE_ROOT" > /dev/null 2>&1 || true
 fi
 
+# handle temporary files & their clean-up
+umask 037
+MDL_ERRORS="$(mktemp -t bbedit_markdownlint)" # will hold DocC errors for processing
+if [ "$?" -ne 0 ] ; then
+	NOTIFY "$SCRIPT: ERROR" "Can't create temp file" 'sosumi'
+	echo "ERROR: Can't create temp file. $SCRIPT (v$VERSION)"
+	exit 1
+fi
+# trap for cleanup (and remove it on the way out to not double-trigger)
+trap 'rm -rf "$MDL_ERRORS" ; trap - EXIT; exit' EXIT INT HUP
 
-if npx markdownlint-cli2 "${BB_DOC_PATH}" > /dev/null 2>&1 ; then
+# markdownlint-cli2 returns: 0=no errors, 1=markdown warnings or errors identified, 2=configuration error or unable to parse
+if markdownlint-cli2 "${BB_DOC_PATH}" > "$MDL_ERRORS" 2>&1 ; then
 	# return code 0 = no errors
 	NOTIFY "$SCRIPT: $SHORTNAME" 'No errors.' 'default'
 	exit 0
-elif [ "$?" -eq 2 ] ; then
-	NOTIFY "$SCRIPT: ERROR" "No results from 'npx markdownlint-cli2 \"$BB_DOC_PATH\", check configuration." 'sosumi'
-	echo "SCRIPT: ERROR: No results from 'npx markdownlint-cli2 \"${BB_DOC_PATH}\". Check configuration. (v$VERSION)"
-	npx markdownlint-cli2 "${BB_DOC_PATH}"
+elif [ "$?" -eq 2 ] || [ ! -s "$MDL_ERRORS" ] ; then
+	# config/processing error code from markdownlint-cli2 OR results file is empty
+	NOTIFY "$SCRIPT: ERROR" "Configuration or other error- no results from 'npx markdownlint-cli2 \"$BB_DOC_PATH\", check configuration." 'sosumi'
+	echo "SCRIPT: ERROR: Configuration or other error- no results from 'npx markdownlint-cli2 \"${BB_DOC_PATH}\". Check configuration. (v$VERSION)"
+	cat "$MDL_ERRORS"
 	exit 1
 fi
-
-RESULTS="$(npx markdownlint-cli2 "${BB_DOC_PATH}" 2>&1)"
-
-# check results
-if [ -z "$RESULTS" ] ; then
-	# no results is an error, there should at least be summary info
-	NOTIFY "$SCRIPT: ERROR" "No results from 'npx markdownlint-cli2 \"$BB_DOC_PATH\", check configuration." 'sosumi'
-	echo "SCRIPT: ERROR: No results from 'npx markdownlint-cli2 \"${BB_DOC_PATH}\". Check configuration. (v$VERSION)"
-	exit 1
-fi
-
-# when the config for markdownlint-cli2 has "noProgress": true"
-# AND there's no errors, results looks like: 'markdownlint-cli2 v0.17.2 (markdownlint v0.37.4)'
-# OR if "noProgress": true" and there's no errors, a summary shows 0 errors
-# shellcheck disable=SC2143
-if [ -z "$(echo "$RESULTS" | grep -Ev 'markdownlint-cli2 v[0-9]+\.[0-9]+\.[0-9]+ \(markdownlint v[0-9]+\.[0-9]+\.[0-9]+\)')" ] || echo "$RESULTS" | grep -Fq 'Summary: 0 error(s)' > /dev/null 2>&1 ; then
-	# no errors, indicate success with terminal notifier and default sound before exiting
-	NOTIFY "$SCRIPT: $SHORTNAME" 'No errors.' 'default'
-	exit 0
-fi
-
-# trim summary or version info from the top to pass bbresults a clean list of issues
-RESULTS="$(echo "$RESULTS" | sed '/^markdownlint-cli2 v/d; /^Linting: /,/^Summary: /d')"
 
 # set BBEdit results RegEx pattern for markdownlint-cli2 default output
 PATTERN='(?P<file>.+?):(?P<line>\d+)?(:(?P<col>\d+))? (?P<type>\S+) (?P<msg>.*)$'
 
-# to debug, uncomment the line below and check the BBEdit "Unix Script Output.log" file.
-# echo "$RESULTS"
-
-# send results back to BBEdit (no need to notify, since Results browser appears)
-echo "$RESULTS" | bbresults --pattern "$PATTERN"
+# trim summary or version info from the top of MDL_ERRORS to make a clean list of issues
+# and pipe results bbresults for BBEdit to show (no need to notify, since BBEdit Results Browser appears)
+sed '/^markdownlint-cli2 v/d; /^Linting: /,/^Summary: /d' "$MDL_ERRORS" | bbresults --pattern "$PATTERN"
